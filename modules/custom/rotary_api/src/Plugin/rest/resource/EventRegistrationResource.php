ab3c5499dd92ee48f9d3a5a4866c9ea3233d57a4
Faheem <faheempmh@gmail.com>
2021-10-26 06:31:47 +0530

    * removed password, email while registration * added validation messages * check for duplicate mobile number

--- modules/custom/rotary_api/src/Plugin/rest/resource/EventRegistrationResource.php
+++ modules/custom/rotary_api/src/Plugin/rest/resource/EventRegistrationResource.php
@@ -7,7 +7,8 @@ use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
+use Drupal\Core\Database\Database as Database;
+use Symfony\Component\HttpFoundation\JsonResponse;
 /**
  * Provides a resource to get view modes by entity and bundle.
  *
@@ -52,14 +53,34 @@ class EventRegistrationResource extends ResourceBase
      *   Throws exception expected.
      */
     public function post($payload)
-    {
+    {   
+        $pincode = random_int(100000, 999999);
+        $errors = [];
+        if($this->isDuplicateUserName($payload["mobile"]))
+        {
+            return new JsonResponse(["error"=>"Mobile number already registered."], 422);
+        }
+        # couple registration
+        if($payload["registrationType"]=="12"){
+            $ann = $payload["dependants"][0] ;
+            if($ann["mobilenumber"]==$payload["mobile"]){
+                return new JsonResponse(["error"=>"Rotarian and Ann's mobile numbers should not be same."], 422);
+            }
+        }
+
+        if(count($errors)>0){
+            return new JsonResponse(["error"=>"duplicate asdeasd"], 422);
+        }
+
+        $recieptId = $this->gerRecieptNumber();
 
         $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
         $user = \Drupal\user\Entity\User::create();
-        $user->setPassword($payload["password"]);
-        $user->setEmail($payload["email"]);
-        $user->setUsername($payload["email"]);
+        $user->setPassword($pincode);
+        $user->setEmail($payload["mobile"]."@raamasambrama.com");
+        $user->setUsername($payload["mobile"]);
         $user->addRole("registrant");
+        $user->set("field_reciept_id", $recieptId);
         $user->set("field_registration_type", $payload["registrationType"]);
         $user->set("field_registrant_name", $payload["registrantName"]);
         $user->set("field_current_designation", $payload["currentDesignation"]);
@@ -69,19 +90,71 @@ class EventRegistrationResource extends ResourceBase
         $user->set("field_contact_address", $payload["contactAddress"]);
         $user->set("field_payment_mode", $payload["paymentMode"]);
         $user->set("field_payment_status", "InformationSubmitted");
+        $user->set("field_password_raw", $pincode);
         $user->set("field_food_preference", $payload["foodprefs"]);
         $user->activate();
+
+        // Couple Registration
+        if($payload["registrationType"]=="12"){
+           
+            $ann = $payload["dependants"][0];
+            if($ann["mobilenumber"]==$payload["mobile"])
+            return new ModifiedResourceResponse(["error" => "duplicate mobile"], 422);
+            if($this->isDuplicateUserName($ann["mobilenumber"]))
+            {
+                return new JsonResponse(["error"=>"Mobile number already registered for Ann."], 422);
+            }
+            $user2 = \Drupal\user\Entity\User::create();
+            $user2->setPassword($pincode);
+            $user2->setEmail($ann["mobilenumber"]."@raamasambrama.com");
+            $user2->setUsername($ann["mobilenumber"]);
+            $user2->addRole("registrant");
+            $user2->set("field_reciept_id", $recieptId);
+            $user2->set("field_password_raw", $pincode);
+            $user2->set("field_registration_type", $payload["registrationType"]);
+            $user2->set("field_registrant_name", $ann["fullname"]);
+            $user2->set("field_current_designation", "Ann");
+            $user2->set("field_mobile", $ann["mobilenumber"]);
+            $user2->set("field_zone", $payload["zoneId"]);
+            $user2->set("field_club", $payload["clubId"]);
+            $user2->set("field_contact_address", $payload["contactAddress"]);
+            $user2->set("field_payment_mode", $payload["paymentMode"]);
+            $user2->set("field_payment_status", "InformationSubmitted");
+            $user2->set("field_food_preference", $ann["foodprefs"]);
+            $user2->activate();
+        }
+        
 
         $user->field_dependants = $this->getDependants($payload);
         $result = [];
         try {
             $result = $user->save();
-            return new ModifiedResourceResponse($user, 200);
+            if($user2!=null)
+            $result2 = $user2->save();
+            return new ModifiedResourceResponse(["message"=>"Registered Successfully"], 200);
         } catch (\Drupal\Core\Entity\EntityStorageException $exception) {
-            return new ModifiedResourceResponse(["error" => $exception->getMessage()], 422);
+            return new ModifiedResourceResponse(["error" => str_contains($exception->getMessage(),"1062 Duplicate entry")?"Mobile number already registered.":$exception->getMessage()], 422);
             // return
         }
 
+    }
+
+    public function gerRecieptNumber(){
+        $db = \Drupal::database();
+        $result = $db->query("select (max(field_reciept_id_value)+1) as reciept_id from user__field_reciept_id");
+        return $result->fetchField();
+    }
+
+    public function isDuplicateUserName($mobile){
+        $db = \Drupal::database();
+        $query = "select count(*) as count from user__field_mobile where field_mobile_value = '$mobile' ";
+        $result = $db->query($query);
+        $count = $result->fetchField();
+
+        if($count>0){
+            return true;
+        }
+        return false;
     }
 
     public function getDependants($payload)
--- themes/custom/rotary_bootstrap/js/registration.js
+++ themes/custom/rotary_bootstrap/js/registration.js
@@ -1,71 +1,77 @@
 const vueAxios = axios.create({
-  baseURL: '/',
-  timeout: 30000
+  baseURL: "/",
+  timeout: 30000,
 });
-
+Vue.use(VeeValidate);
 var app = new Vue({
-  el: '#app',
-  data:
-    function () {
-      return {
-        formData: {
-          registrantName: "",
-          email: "",
-          password: "",
-          confirmpassword: "",
-          registrationType: 0,
-          clubId: "",
-          zoneId: "",
-          paymentMode: "",
-          mobile: "",
-          contactAddress: "",
-          currentDesignation: "",
-          totalAmount: "",
-          dependants: []
-        },
-        zones: [],
-        clubs: [],
-        registrationTypes: [],
-        csrfToken: "",
-        selectedRegistrationType: null
-      }
-    },
+  el: "#app",
+  data: function () {
+    return {
+      formData: {
+        registrantName: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        registrationType: 0,
+        clubId: "",
+        zoneId: "",
+        paymentMode: "",
+        mobile: "",
+        contactAddress: "",
+        currentDesignation: "",
+        totalAmount: "",
+        dependants: [],
+      },
+      zones: [],
+      clubs: [],
+      registrationTypes: [],
+      csrfToken: "",
+      selectedRegistrationType: null,
+    };
+  },
   mounted() {
-    this.loadCsrfToken()
-    this.loadZones()
-    this.loadRegistrationTypes()
+    this.loadCsrfToken();
+    this.loadZones();
+    this.loadRegistrationTypes();
   },
   methods: {
     loadZones: async function () {
-      var response = await vueAxios.get("/api/list/zones?_format=json")
-      this.zones = response.data
+      var response = await vueAxios.get("/api/list/zones?_format=json");
+      this.zones = response.data;
     },
     loadClubs: async function (event) {
-      var response = await vueAxios.get(`/api/list/clubs/${this.formData.zoneId}?_format=json`)
-      this.clubs = response.data
-
+      var response = await vueAxios.get(
+        `/api/list/clubs/${this.formData.zoneId}?_format=json`
+      );
+      this.clubs = response.data;
     },
     loadRegistrationTypes: async function (event) {
-      var response = await vueAxios.get(`/api/registration-types?_format=json`)
-      this.registrationTypes = response.data
+      var response = await vueAxios.get(`/api/registration-types?_format=json`);
+      this.registrationTypes = response.data;
     },
     selectRegistrationType: async function (event) {
-
-      this.selectedRegistrationType = this.registrationTypes.find(item => item.tid == event.target.value)
+      this.selectedRegistrationType = this.registrationTypes.find(
+        (item) => item.tid == event.target.value
+      );
       this.formData["registrationType"] = this.selectedRegistrationType["tid"];
     },
-    submitRegistration: async function () {
-      try {
-        if (this.formData["password"] != this.formData["confirmpassword"]) {
-          alert("Password & Confirm Password do not match")
-          return;
+    submitRegistration: function () {
+      this.$validator.validate().then(async (valid) => {
+        try {
+          if (valid) {
+            var response = await vueAxios.post(
+              `/event/registration`,
+              this.formData
+            );
+            window.location = "/complete-message";
+          }
+        } catch (err) {
+          if(err.response.data["error"])
+          {
+            alert(err.response.data["error"]);
+          }
         }
-        var response = await vueAxios.post(`/event/registration`, this.formData)
-        window.location = "/complete-message"
-      }
-      catch (err) {
-
-      }
+      });
     },
     loadCsrfToken() {
       vueAxios.get("/session/token").then((response) => {
@@ -78,12 +84,16 @@ var app = new Vue({
     dependantsCount: function () {
       this.formData.dependants = [];
       if (this.selectedRegistrationType == null) {
-        return 0
+        return 0;
       }
-      for (var i = 0; i < parseInt(this.selectedRegistrationType["totalMembers"]); i++) {
+      for (
+        var i = 0;
+        i < parseInt(this.selectedRegistrationType["totalMembers"]);
+        i++
+      ) {
         this.formData.dependants.push({});
       }
-      return parseInt(this.selectedRegistrationType["totalMembers"])
-    }
-  }
-})
+      return parseInt(this.selectedRegistrationType["totalMembers"]);
+    },
+  },
+});
