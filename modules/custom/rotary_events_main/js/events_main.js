33262bf0d5b9e431cfa92673d41a66b29fe6e605
Faheem <faheempmh@gmail.com>
2021-12-16 08:04:28 +0530

    Added JQUERY code to filter Club Based on zones

---
+++ modules/custom/rotary_events_main/js/events_main.js
@@ -0,0 +1,18 @@
+(function ($, Drupal) {
+    $("#edit-field-zone-target-id").on("change",function(e){
+        var target = e.target
+        var zone = target.value
+        $.ajax({
+            url:"/api/list/clubs/"+zone+"?_format=json",
+            type:"GET",
+            success: function(data){
+                var clubsDropdown = $("#edit-field-club-target-id")
+                clubsDropdown.find('option').remove();
+                for(index in data){
+                    var club = data[index]
+                    clubsDropdown.append("<option value="+club["tid"]+">"+club["name"]+"</option>")
+                }
+            }
+        })
+    })
+})(jQuery, Drupal);
